83e746b2b96020eb4b8a57b3e0e304cb8e6d1e9c
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2026-04-08 14:17:25 -0500

    Add Sponsor entity and SponsorCategory enum

---
+++ SportsLeague.Domain/Entities/Sponsor.cs
@@ -0,0 +1,20 @@
+﻿using SportsLeague.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsLeague.Domain.Entities
+{
+    public class Sponsor : AuditBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public string ContactEmail { get; set; } = string.Empty;
+        public string? Phone { get; set; }
+        public string? WebsiteUrl { get; set; }
+        public SponsorCategory Category { get; set; }
+        // Navigation Property para la relación N:M
+        public ICollection<TournamentSponsor> TournamentSponsors { get; set; } = new List<TournamentSponsor>();
+    }
+}
